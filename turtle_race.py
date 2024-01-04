6b1a8558673c182f34d2a1dbcea20c1713e860db
Elvo12 <147548984+Elvo12@users.noreply.github.com>
2024-01-04 05:08:45 +0000

    Add files via upload

---
+++ turtle_race.py
@@ -0,0 +1,17 @@
+from turtle import *
+elvis = Turtle()
+elvis.color('pink')
+elvis.shape('turtle')
+elvis.penup
+elvis.goto(-160, 100)
+elvis.pendown
+
+
+elv = Turtle()
+elv.color('red')
+elv.shape('turtle')
+elv.penup
+elv.goto(-190, 69)
+elv.pendown
+x = input("Press enter to close")
+
